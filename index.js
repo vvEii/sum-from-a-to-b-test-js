43c909e8e1bfc1f57eb8cd4c20d084fb6a1ea1f1
shangguanwei <coopershang125@gmail.com>
2020-09-19 17:36:07 +0000

    recursion

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+const sum = function(fromN, toN) {
+  if (fromN === toN) {
+    return toN;
+  }
+  return fromN += sum(fromN + 1, toN);
+};
 
 module.exports = sum;
